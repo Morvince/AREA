f76445504eae871ea2ac689c38b200147617947f
NassimAlaimi <nassim.alaimi@epitech.eu>
2023-01-17 10:34:10 +0100

    [ADD]: Navbar wip

--- area/src/pages/index.js
+++ area/src/pages/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Sidebar from '../components/sidebar'
+import Navebar from '../components/navbar'
 
 const Home = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div>
-      
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navebar toggle={toggle} changeY={720} defaultState={false}/>
     </div>
   )
 }
