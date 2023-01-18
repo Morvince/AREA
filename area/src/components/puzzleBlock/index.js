362a032a68ad7581a52a92922288255a05e6192a
Enzo Bonato <enzo.bonato@epitech.eu>
2023-01-18 14:55:44 +0100

    [EDIT]: Change colors dans cut function

--- area/src/components/puzzleBlock/index.js
+++ area/src/components/puzzleBlock/index.js
@@ -1,14 +1,44 @@
-import {Rectangle} from "./puzzleBlockElements"
+import React, { useState, useRef } from 'react';
+import { Rectangle, Barre} from './puzzleBlockElements';
+
+const Mouvement = (props) => {
+  const [position, setPosition] = useState({ x: props.x, y: props.y });
+  const ref = useRef(null);
+
+  const handleMouseDown = (e) => {
+    const initialX = e.clientX - position.x;
+    const initialY = e.clientY - position.y;
+
+    const handleMouseMove = (e) => {
+      setPosition({
+        x: e.clientX - initialX,
+        y: e.clientY - initialY
+      });
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    const handleMouseUp = () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    }
+
+    window.addEventListener('mouseup', handleMouseUp);
+  }
+
+  return (
+    <div ref={ref}>
+      <Rectangle x={position.x + "px"} y={position.y + "px"} color={props.color} onMouseDown={handleMouseDown}/>
+    </div>
+  );
+}
 
 const PuzzeBlock = () => {
-    return (
-      <>
-        <Rectangle x={350} y={150} color='#784ecc'/>
-        <Rectangle x={350} y={150} color='#784ecc'/>
-        <Rectangle x={350} y={150} color='#784ecc'/>
-        <Rectangle x={350} y={150} color='#784ecc'/>
-        <Rectangle x={350} y={150} color='#784ecc'/>
-      </>
-    )
-  }
+  return (
+    <div>
+      <Barre />
+      <Mouvement x="300" y="150" color='#686f84'/>
+    </div>
+  )
+}
   export default PuzzeBlock
--- area/src/components/puzzleBlock/puzzleBlockElements.js
+++ area/src/components/puzzleBlock/puzzleBlockElements.js
@@ -1,44 +1,23 @@
-import React, { useState, useRef } from 'react';
+import styled from "styled-components";
 
-export const Rectangle = (props) => {
-  const [position, setPosition] = useState({ x: props.x, y: props.y });
-  const ref = useRef(null);
+export const Rectangle = styled.div`
+    width: 250px;
+    height: 125px;
+    position: absolute;
+    top: ${position => position.y};
+    left: ${position => position.x};
+    background: ${props => props.color};
+    border-radius: 25px;
+    /* border: 3px solid black; */
+    box-shadow: 5px 5px 1px 0 #373b48
+;
+`;
 
-  const handleMouseDown = (e) => {
-    const initialX = e.clientX - position.x;
-    const initialY = e.clientY - position.y;
-
-    const handleMouseMove = (e) => {
-      setPosition({
-        x: e.clientX - initialX,
-        y: e.clientY - initialY
-      });
-    }
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    const handleMouseUp = () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-      window.removeEventListener('mouseup', handleMouseUp);
-    }
-
-    window.addEventListener('mouseup', handleMouseUp);
-  }
-
-  return (
-    <div
-      ref={ref}
-      onMouseDown={handleMouseDown}
-      style={{
-        width: '250px',
-        height: '150px',
-        position: 'absolute',
-        left: position.x,
-        top: position.y,
-        background: props.color,
-        borderRadius: '20%',
-        border: '3px solid black',
-      }}
-    />
-  );
-}
+export const Barre = styled.div`
+    height: 10000px;
+    width: 5px;
+    position: absolute;
+    top: 0px;
+    left: 600px;
+    background: black
+`;
