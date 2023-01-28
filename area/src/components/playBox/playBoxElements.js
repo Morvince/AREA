3dd0f272305bfae2f0c9520d23f6d005f5d3981b
Nassim <nassim.alaimi@epitech.eu>
2023-01-28 17:54:16 +0100

    [RM]: Remove useless comment

--- area/src/components/playBox/playBoxElements.js
+++ area/src/components/playBox/playBoxElements.js
@@ -11,10 +11,6 @@ export const RectangleArea = styled.div`
 export const MovableBox = styled.div`
   display: flex;
   flex-grow: 2;
-  /* position: absolute; */
-  /* width: 100%; */
-  /* height: 90%; */
-  /* margin: 5px 5px 85px 5px; */
   background: green;
   border-radius: 25px;
   box-shadow: 5px 5px 1px 0 #373b48;
