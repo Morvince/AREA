68e8370d6ec727e7a72d19a7c5256946eeeb73fd
noaleclaire <noa.leclaire@epitech.eu>
2023-02-02 11:00:46 +0100

    [UPDATE] : PrivateRoutes that correctly redirect to home

--- area/src/utils/PrivateRoutes.js
+++ area/src/utils/PrivateRoutes.js
@@ -1,7 +1,6 @@
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoutes = () => {
-  console.log(sessionStorage.getItem("token"))
   return (
     sessionStorage.getItem("token") ? <Outlet/> : <Navigate to="/sign"/>
   )
