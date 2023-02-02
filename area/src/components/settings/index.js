8437281a6cfb7ead104e622015b5b4b90fbb9125
Enzo Bonato <enzo.bonato@epitech.eu>
2023-02-02 15:50:15 +0100

    [EDIT]: Edit position of icon for fireworks

--- area/src/components/settings/index.js
+++ area/src/components/settings/index.js
@@ -10,15 +10,15 @@ const Settings = () => {
         <SettingsRect top="200px" height="600px" color="#D4D3DC" width="700px" left="50px" SettingsRect/>
         <Icon icon="skill-icons:discord" width="100" style={{ position: 'absolute', left: '100px', top: "250px" }}/>
         <Icon icon="logos:spotify-icon" width="100" style={{ position: 'absolute', left: '100px', top: "450px" }}/>
-        <Icon icon="skill-icons:instagram" width="100" style={{ position: 'absolute', left: '100', top: "650px" }}/>
+        <Icon icon="skill-icons:instagram" width="100" style={{ position: 'absolute', left: '100px', top: "650px" }}/>
         <Button to="/" top="265px" left="400px" >Connect</Button>
         <Button to="/" top="465px" left="400px" >Connect</Button>
         <Button to="/" top="665px" left="400px" >Connect</Button>
 
         <SettingsRect top="200px" height="600px" color="#D4D3DC" width="700px" left="950px" SettingsRect/>
-        <Icon icon="logos:google-icon" width="100" style={{ position: 'absolute', left: '1000', top: "250px" }}/>
-        <Icon icon="skill-icons:twitter" width="100" style={{ position: 'absolute', left: '1000', top: "450px" }}/>
-        <Icon icon="logos:openai-icon" width="100" style={{ position: 'absolute', left: '1000', top: "650px" }}/>
+        <Icon icon="logos:google-icon" width="100" style={{ position: 'absolute', left: '1000px', top: "250px" }}/>
+        <Icon icon="skill-icons:twitter" width="100" style={{ position: 'absolute', left: '1000px', top: "450px" }}/>
+        <Icon icon="logos:openai-icon" width="100" style={{ position: 'absolute', left: '1000px', top: "650px" }}/>
         <Button to="/" top="265px" left="1300px" >Connect</Button>
         <Button to="/" top="465px" left="1300px" >Connect</Button>
         <Button to="/" top="665px" left="1300px" >Connect</Button>
