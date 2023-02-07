54418265f093fe6c718c91fe4fd8537c32cd1c51
noaleclaire <noa.leclaire@epitech.eu>
2023-02-07 21:54:57 +0100

    [ADD]: icons of 3 services

--- client_mobile/App.js
+++ client_mobile/App.js
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Sign">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Sign" component={Sign}/>
         <Stack.Screen name="Home" component={Home}/>
       </Stack.Navigator>
--- client_mobile/src/pages/Home.js
+++ client_mobile/src/pages/Home.js
@@ -1,9 +1,13 @@
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Fontisto } from '@expo/vector-icons';
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <Fontisto name="discord" size={70} color="#7289da" />
+      <Fontisto name="spotify" size={70} color="#17d860" />
+      <Fontisto name="twitter" size={70} color="#179cf0" />
       {/* <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
