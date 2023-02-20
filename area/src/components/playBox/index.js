c1ca5062e5a77b75243790bb2863c7ea1a5c888c
Enzo Bonato <enzo.bonato@epitech.eu>
2023-02-20 11:48:32 +0100

    [ADD]: Add IslinkedListEmpty

--- area/src/components/playBox/index.js
+++ area/src/components/playBox/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { RectangleArea, MovableBox, TickButton, ValidateButton, BinLeft, BinRight, BinWhite } from './playBoxElements'
+import { RectangleArea, MovableBox, ValidateButton, BinLeft, BinRight, BinWhite } from './playBoxElements'
 import Servicesbar from '../servicesbar'
 import Block from '../block'
 import MyContext from '../Context'
 import { useGetUserPlaylist } from '../../api/apiSpotify';
 import { useEditAutomation } from '../../api/apiServicesPage';
-import { Redirect, useNavigate } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 
 const PlayBox = (props) => {
@@ -16,52 +15,59 @@ const PlayBox = (props) => {
   const automationId = props.automationId;
   const userPlaylist = useGetUserPlaylist();
   const editAutomation = useEditAutomation();
-  const navigate = useNavigate();
-  const iconColor = (sharedData).length > 1 ? 'green' : 'red';
   const { onValidate } = props;
+  const [isLinkedListEmpty, setIsLinkedListEmpty] = React.useState(true);
 
   React.useEffect(() => {
+    console.log(linkedList);
+    if (linkedList.length > 1) {
+      setIsLinkedListEmpty(false);
+    } else {
+      setIsLinkedListEmpty(true);
+    }
     userPlaylist.mutate()
     if (userPlaylist.isSuccess) {
       setPlaylist(userPlaylist.data.data);
     }
-  }, [sharedData]);
+
+  }, [sharedData, linkedList]);
 
   function sendAutomation() {
     var actions = [];
-    var i = {id: 0, number: 0, informations: {}};
+    var i = { id: 0, number: 0, informations: {} };
 
     onValidate();
     for (var j = 0; j < linkedList.length; j++) {
       i.id = sharedData[j].dbId;
-      i.number = j+1;
+      i.number = j + 1;
       i.informations = sharedData[j].toSend;
       actions.push(i);
-      i = {id: 0, number: 0, informations: {}}
+      i = { id: 0, number: 0, informations: {} }
     }
-    editAutomation.mutate({id: automationId, actions: actions})
+    editAutomation.mutate({ id: automationId, actions: actions })
     setSharedData([]);
+    setLinkedList([]);
   }
 
   return (
     <RectangleArea>
       <BinLeft> </BinLeft>
       <MyContext.Provider value={{ sharedData, setSharedData, ID, setID, linkedList, linkedList, setLinkedList, playlist, setPlaylist }}>
-      <Servicesbar />
-      <MovableBox>
-        {sharedData.map((info) => {
-          return (
-            <Block key={info.index} id={info.index} top={info.top} left={info.left} color={info.color} service={info.service} action={info.action} name={info.name} nbrBox={info.nbrBox} />
-            )
+        <Servicesbar />
+        <MovableBox>
+          {sharedData.map((info) => {
+            return (
+              <Block key={info.index} id={info.index} top={info.top} left={info.left} color={info.color} service={info.service} action={info.action} name={info.name} nbrBox={info.nbrBox} />
+              )
           })}
-      </MovableBox>
-    </MyContext.Provider>
-    <BinRight></BinRight>
-    <BinWhite></BinWhite>
-    <Icon icon="mdi:delete-circle-outline" color="#373b48" width="40" style={{position: 'absolute', top: '20%', left: '80.3%'}}/>
-    <ValidateButton className={iconColor === 'green' ? 'green' : 'red'} onClick={sendAutomation} disabled={iconColor === 'red'}>
-      <Icon icon="material-symbols:playlist-add-check-circle" width="100" color={iconColor} />
-    </ValidateButton>
+        </MovableBox>
+      </MyContext.Provider>
+      <BinRight></BinRight>
+      <BinWhite></BinWhite>
+      <Icon icon="mdi:delete-circle-outline" color="#373b48" width="40" style={{ position: 'absolute', top: '20%', left: '80.3%' }} />
+      <ValidateButton className={isLinkedListEmpty === false ? 'green' : 'red'} onClick={sendAutomation} disabled={isLinkedListEmpty === true}>
+        <Icon icon="material-symbols:playlist-add-check-circle" width="100" color={isLinkedListEmpty === false ? 'green' : 'red'} />
+      </ValidateButton>
     </RectangleArea>
   )
 }
