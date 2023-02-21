68b9851960de2476925e73911eb263f155e53f47
Laurent Enzo <enzo.laurent@epitech.eu>
2023-02-21 18:03:58 +0100

    [ADD]: first gmail action, when a new mail is in the inbox

--- server/src/Controller/GmailAPIController.php
+++ server/src/Controller/GmailAPIController.php
@@ -202,12 +202,78 @@
             if (empty($this->request_api)) {
                 $this->request_api = new RequestAPI();
             }
-            $response = $this->request_api->send($access_token, self::API_URL . $endpoint, $method, $parameters);
-            if (isset(json_decode($response)->error)) {
-                $response = json_encode(array("message" => "Gmail: ".json_decode($response)->error->status, "code" => json_decode($response)->error->code));
+            $response = json_decode($this->request_api->send($access_token, self::API_URL . $endpoint, $method, $parameters));
+            if (isset($response->error)) {
+                $response = array("message" => "Gmail: ".$response->error->status, "code" => $response->error->code);
             }
             return $response;
         }
+
+        // Action
+        /**
+         * @Route("/gmail/action/check_mail", name="gmail_api_action_check_mail")
+         */
+        public function hasInboxNewMail(Request $request)
+        {
+            // Get needed values
+            $request_content = json_decode($request->getContent());
+            if (empty($request_content->new) || empty($request_content->old)) {
+                return new JsonResponse(array("message" => "Gmail: Missing field"), 400);
+            }
+            $old_mails = $request_content->old;
+            $new_mails = $request_content->new;
+            // Check if a new mail is in the inbox
+            foreach ($new_mails as $new_mail) {
+                $found = false;
+                foreach ($old_mails as $old_mail) {
+                    if (strcmp($new_mail->id, $old_mail->id) === 0) {
+                        $found = true;
+                        break (1);
+                    }
+                }
+                if ($found === false) {
+                    return new JsonResponse(array("message" => true), 200);
+                }
+            }
+            return new JsonResponse(array("message" => false), 200);
+        }
+        /**
+         * @Route("/gmail/action/check_mail/get_parameters", name="gmail_api_action_check_mail_parameters")
+         */
+        public function getHasInboxNewMailParameters(Request $request, AutomationRepository $automation_repository, AutomationActionRepository $automation_action_repository, ServiceRepository $service_repository, UserServiceRepository $user_service_repository)
+        {
+            // Get needed values
+            $request_content = json_decode($request->getContent());
+            if (empty($request_content->automation_action_id)) {
+                return new JsonResponse(array("message" => "Gmail: Missing field"), 400);
+            }
+            $automation_action_id = $request_content->automation_action_id;
+            $automation_action = $automation_action_repository->find($automation_action_id);
+            if (empty($automation_action)) {
+                return new JsonResponse(array("message" => "Gmail: automation_action ID not found"), 404);
+            }
+            $service = $service_repository->findByName("gmail");
+            if (empty($service)) {
+                return new JsonResponse(array("message" => "Gmail: Service not found"), 404);
+            }
+            $service = $service[0];
+            $automation = $automation_repository->find($automation_action->getAutomationId());
+            if (empty($user_service_repository->findByUserIdAndServiceId($automation->getUserId(), $service->getId()))) {
+                return new JsonResponse(array("message" => "Gmail: Access token not found"), 404);
+            }
+            $access_token = $user_service_repository->findByUserIdAndServiceId($automation->getUserId(), $service->getId())[0]->getAccessToken();
+            $informations = $automation_action->getInformations();
+            if (empty($informations->playlist_id)) {
+                return new JsonResponse(array("message" => "Gmail: Playlist ID not found"), 404);
+            }
+            // Request to get the 10 last mails
+            $response = $this->sendRequest($access_token, "users/me/messages?maxResults=10");
+            if (isset($response->code)) {
+                return new JsonResponse(array("message" => $response->message), $response->code);
+            }
+            return new JsonResponse($response, 200);
+        }
+        
         /**
          * @Route("/gmail/test", name="gmail_api_test")
          */
@@ -216,7 +282,8 @@
             $user_id = 1;
             $service_id = $service_repository->findByName("gmail")[0]->getId();
             $access_token = $user_service_repository->findByUserIdAndServiceId($user_id, $service_id)[0]->getAccessToken();
-            $response = json_decode($this->sendRequest($access_token, "users/me/messages?maxResults=10"));
+            // $response = $this->sendRequest($access_token, "users/me/messages?maxResults=10");a vérifier avec un vrai compte
+            $response = array();//pour le from c'est dans $mail->payload->headers[] if ->name == "From" l'email = ->value
             return new JsonResponse(array("message" => $response));
         }
     }
