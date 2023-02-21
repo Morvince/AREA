d6e1e0a19d66921272e55d82d85f7c4eb00ee97a
NassimAlaimi <nassim.alaimi@epitech.eu>
2023-02-21 10:30:25 +0100

    [FIX]: remove useless lib

--- area/src/components/infoBlock/index.js
+++ area/src/components/infoBlock/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { InfoBlockContainer, InfoWrapper, InfoTitle, InfoAction, InputBox } from './infoBlockElements'
-import Dropdown from 'react-bootstrap/Dropdown';
 
 const InfoBlock = () => {
   return (
@@ -8,17 +7,7 @@ const InfoBlock = () => {
       <InfoWrapper>
         <InfoTitle>Titre</InfoTitle>
         <InfoAction>
-          <InputBox id="name" type="text" placeholder="Name"/>
-          <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Action
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+          <InputBox id="mail" type="text" placeholder="Name"/>
         </InfoAction>
       </InfoWrapper>
     </InfoBlockContainer>
