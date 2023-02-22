35fc71633530a8c36df8a855ebb2239fb44eacb0
Enzo Bonato <enzo.bonato@epitech.eu>
2023-02-22 09:42:12 +0100

    [ADD]: Change way to display AreasZone

--- area/src/components/areas/areasElements.js
+++ area/src/components/areas/areasElements.js
@@ -7,115 +7,118 @@ export const BgColor = styled.div`
     background: #373b48;
 `;
 
-export const BlankZone = styled.div`
-  width: 80%;
-  height: 80%;
-  position: fixed;
-  top: 15%;
-  left: 10%;
-  background: white;
+
+export const AreaZone = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background: grey;
+  height: 100px;
+  margin-bottom: 75px;
+  border: 2px solid black;
+`;
+export const GlobalContainer = styled.div`
+  width: 90%;
+  height: 200%;
+  top: 40%;
+  left: 5%;
+  position: absolute;
+  margin-bottom: 100px;
   display: flex;
   flex-direction: column;
+  /* background: red; */
 `;
 
 export const NumberOfAreasText = styled.div`
-    position: absolute;
-    font-size: 30px;
-    top: 10%;
-    left: 32.5%;
-    color: black;
-    user-select: none;
-`;
-
-export const AreaZone = styled.div`
-  background: grey;
-  position: relative;
-  top: 25%;
-  height: 10%;
-  width: 99%;
-  left: 0.5%;
-  margin-bottom: 20px;
-  border: 2px solid black;
-`;
-
-export const Container = styled.div`
   position: absolute;
-  top: 110%;
-  left: 0;
+  font-size: 30px;
+  top: 20%;
+  left: 35%;
+  color: white;
+  user-select: none;
+  font-weight: bold;
+`;
+
+export const BoxContent = styled.div`
+  position: relative;
+  display: flex;
   width: 100%;
-  height: 300px;
-  background-color: grey;
+  height: 400%;
+  background: grey;
+  margin-top: 600px;
+  margin-bottom: 100px;
   border: 2px solid black;
-  padding: 10px;
+  color: white;
 `;
 
 export const AreaName = styled.div`
-    position: absolute;
-    background-color: black;
-    color: white;
-    font-size: 50px;
-    font-weight: bold;
-    display: inline-block;
-    user-select: none;
-    padding: 10px;
-    `;
+  position: absolute;
+  background-color: black;
+  color: white;
+  font-size: 50px;
+  font-weight: bold;
+  display: inline-block;
+  user-select: none;
+  padding: 10px;
+`;
 
 export const ButtonEdit = styled.div`
-    /* button */
-    position: absolute;
-    background-color: rgba(0, 0, 0, 0.6);
-    height: 20%;
-    width: 10%;
-    left : 79%;
-    top: 79%;
-    
-    /* text */
-    color: white;
-    text-align: center;
-    font-size: 40px;
-    font-weight: bold;
-    display: inline-block;
-    user-select: none;
-    padding: 10px;
+  /* button */
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.6);
+  height: 20%;
+  width: 10%;
+  left : 79%;
+  top: 79%;
+  
+  /* text */
+  color: white;
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+  display: inline-block;
+  user-select: none;
+  padding: 10px;
 
-    &:hover {
-    background-color: rgba(0, 0, 0, 1);
-}
+  &:hover {
+  background-color: rgba(0, 0, 0, 1);
+  }
 `;
 
 export const ButtonDelete = styled.div`
-    /* button */
-    position: absolute;
-    background-color: rgba(255, 12, 0, 0.5);
-    height: 20%;
-    width: 10%;
-    left : 89.5%;
-    top: 79%;
-    
-    /* text */
-    color: white;
-    text-align: center;
-    font-size: 40px;
-    font-weight: bold;
-    display: inline-block;
-    user-select: none;
-    padding: 10px;
+  /* button */
+  position: absolute;
+  background-color: rgba(255, 12, 0, 0.5);
+  height: 20%;
+  width: 10%;
+  left : 89.5%;
+  top: 79%;
+  
+  /* text */
+  color: white;
+  text-align: center;
+  font-size: 40px;
+  font-weight: bold;
+  display: inline-block;
+  user-select: none;
+  padding: 10px;
 
-    &:hover {
-    background-color: rgba(255, 12, 0, 1);
-    }
+  &:hover {
+  background-color: rgba(255, 12, 0, 1);
+  }
 `;
 
 export const ArrowArea = styled.div`
-    position: absolute;
-    background-color: black;
-    height: 100%;
-    width: 5%;
-    left : 95%;
-    opacity: 0.7;
+  position: absolute;
+  background-color: black;
+  height: 100%;
+  width: 5%;
+  left : 95%;
+  opacity: 0.7;
 
-    &:hover {
-        opacity: 1;
-        cursor: pointer;
-    }
+  &:hover {
+      opacity: 1;
+      cursor: pointer;
+  }
 `;
--- area/src/components/areas/index.js
+++ area/src/components/areas/index.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
-import { AreaName, AreaZone, ArrowArea, BgColor, BlankZone, ButtonDelete, ButtonEdit, NumberOfAreasText, Container } from './areasElements';
+import { AreaName, AreaZone, ArrowArea, BgColor, ButtonDelete, ButtonEdit, NumberOfAreasText, GlobalContainer, BoxContent } from './areasElements';
 import { Icon } from '@iconify/react';
 
 const EditAreas = () => {
-  const [areas, setAreas] = useState(Array.from({ length: 4 }, (_, index) => index + 1));
+  const [areas, setAreas] = useState(Array.from({ length: 1 }, (_, index) => index + 1));
   const [openArea, setOpenArea] = useState(-1);
 
   const onArrowClick = (index) => {
@@ -11,20 +11,19 @@ const EditAreas = () => {
   };
   return (
     <>
-      <BgColor> 
-        <BlankZone>
-          <NumberOfAreasText> You have exactly {areas.length} Area{areas.length !== 1 ? 's' : ''} running! </NumberOfAreasText>
-          {areas.map((area, index) => (
-            <AreaZone key={`Area${index}`}>
-              <AreaName> Area{area}</AreaName>
-              <ArrowArea onClick={() => onArrowClick(index)}> 
-                <Icon icon="material-symbols:arrow-drop-down-sharp" width="90" style={{ position: 'absolute', color: "white", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} />
-              </ArrowArea>
-              {openArea === index && <Container>Container for Area{area}</Container>}
-            </AreaZone>
-          ))}
-        </BlankZone>
-      </BgColor>
+      <BgColor BgColor/>
+        <NumberOfAreasText> You have exactly {areas.length} Area{areas.length !== 1 ? 's' : ''} running! </NumberOfAreasText>
+      <GlobalContainer> 
+        {areas.map((area, index) => (
+          <AreaZone key={`Area${index}`}>
+            {/* <AreaName> Area{area}</AreaName> */}
+            <ArrowArea onClick={() => onArrowClick(index)}> 
+              <Icon icon="material-symbols:arrow-drop-down-sharp" width="90" style={{ position: 'absolute', color: "white", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} />
+            </ArrowArea>
+            {openArea === index && <BoxContent>Container for Area{area}</BoxContent>}
+          </AreaZone>
+        ))}
+        </GlobalContainer>
     </>
   );
 };
