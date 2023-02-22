83b571b16bc21c58cb79d289d5e5e4e94151d45c
Enzo Bonato <enzo.bonato@epitech.eu>
2023-02-22 18:02:56 +0100

    [ADD]: change front

--- area/src/components/areas/areasElements.js
+++ area/src/components/areas/areasElements.js
@@ -17,6 +17,7 @@ export const AreaZone = styled.div`
   width: 100%;
   border: 2px solid black;
   top: ${props => props.top};
+  border-radius: 30px;
 `;
 
 export const GlobalContainer = styled.div`
@@ -50,6 +51,7 @@ export const BoxContent = styled.div`
   margin-bottom: 100px;
   border: 2px solid black;
   color: white;
+  border-radius: 30px;
 `;
 
 export const AreaName = styled.div`
@@ -61,7 +63,8 @@ export const AreaName = styled.div`
   display: inline-block;
   user-select: none;
   padding: 20px;
-  left: 0%;
+  left: -0.1%;
+  border-radius: 30px 0px 0px 30px;
 `;
 
 export const ButtonEdit = styled.div`
@@ -70,8 +73,9 @@ export const ButtonEdit = styled.div`
   background-color: rgba(0, 0, 0, 0.6);
   height: 20%;
   width: 10%;
-  left : 80%;
+  left : 80.1%;
   top: 80%;
+  border-radius: 30px 0px 0px 0px;
   
   /* text */
   color: white;
@@ -88,14 +92,24 @@ export const ButtonEdit = styled.div`
   }
 `;
 
+export const CutBarre = styled.div`
+  position: absolute;
+  background-color: black;
+  height: 100%;
+  width: 0.2%;
+  left : 30%;
+  top: 0%;
+`;
+
 export const ButtonDelete = styled.div`
   /* button */
   position: absolute;
   background-color: rgba(255, 12, 0, 0.5);
   height: 20%;
   width: 10%;
-  left : 90%;
+  left : 90.1%;
   top: 80%;
+  border-radius: 0px 0px 30px 0px;
   
   /* text */
   color: white;
@@ -117,8 +131,10 @@ export const ArrowArea = styled.div`
   background-color: black;
   height: 100%;
   width: 5%;
-  left : 95%;
+  left : 95.2%;
   opacity: 0.7;
+  border-radius: 0px 30px 30px 0px;
+
 
   &:hover {
       opacity: 1;
--- area/src/components/areas/index.js
+++ area/src/components/areas/index.js
@@ -1,35 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { AreaName, AreaZone, ArrowArea, BgColor, ButtonDelete, ButtonEdit, NumberOfAreasText, GlobalContainer, BoxContent } from './areasElements';
+import { AreaName, AreaZone, ArrowArea, BgColor, ButtonDelete, ButtonEdit, NumberOfAreasText, GlobalContainer, BoxContent, CutBarre } from './areasElements';
 import { Icon } from '@iconify/react';
 import { useGetInfosAreas } from '../../api/apiAreasPage';
 
 const EditAreas = () => {
-  const [areas, setAreas] = useState(Array.from({ length: 1}, (_, index) => index + 1));
   const [openArea, setOpenArea] = useState(-1);
   const [infosFromDb, setInfosFromDb] = useState([]);
   const getInfosFromDb = useGetInfosAreas();
 
-  const containerHeight = (areas.length * 175 + (openArea !== -1 ? 400 : 0)) + 'px';
-
   useEffect(() => {
-    getInfosFromDb.mutate(null, {onSuccess: (data)=> {setInfosFromDb(data)}});
-  },[]);
-  console.log(infosFromDb);
+    getInfosFromDb.mutate(null, { onSuccess: (data) => { setInfosFromDb(data) } });
+  }, []);
+
+  const numberOfAreas = infosFromDb.data?.automations?.length || 0;
+
+  const areas = Array.from({ length: numberOfAreas }, (_, index) => index + 1);
+  const containerHeight = (areas.length * 175 + (openArea !== -1 ? 400 : 0)) + 'px';
 
   return (
     <>
-      <BgColor BgColor/>
-      <NumberOfAreasText> You have exactly {areas.length} Area{areas.length !== 1 ? 's' : ''} running! </NumberOfAreasText>
-      <GlobalContainer height={containerHeight}> 
+      <BgColor BgColor />
+      <NumberOfAreasText> You have exactly {numberOfAreas} Area{numberOfAreas !== 1 ? 's' : ''} running! </NumberOfAreasText>
+      <GlobalContainer height={containerHeight}>
         {areas.map((area, index) => {
           const top = index * 175 + (index > openArea ? (openArea !== -1 ? 400 : 0) : 0);
           return (
             <AreaZone top={top + "px"} key={`area${area}`}>
-              <AreaName> Area{area}</AreaName>
-              <ArrowArea onClick={() => setOpenArea(openArea === index ? -1 : index)}> 
-                <Icon icon="material-symbols:arrow-drop-down-sharp" width="90" style={{ position: 'absolute', color: "white", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}} />
+              <CutBarre> </CutBarre> 
+              <AreaName> Area {area}</AreaName>
+              <ArrowArea onClick={() => setOpenArea(openArea === index ? -1 : index)}>
+                <Icon icon="material-symbols:arrow-drop-down-sharp" width="90" style={{ position: 'absolute', color: "white", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }} />
               </ArrowArea>
-              {openArea === index && <BoxContent>Container for Area{area} <ButtonDelete> Delete</ButtonDelete> <ButtonEdit> Edit </ButtonEdit></BoxContent>}
+              {openArea === index && <BoxContent>Conteneur pour l'area {area}
+                <ButtonDelete> Delete </ButtonDelete>
+                <ButtonEdit> Edit </ButtonEdit>
+              </BoxContent>}
             </AreaZone>
           );
         })}
