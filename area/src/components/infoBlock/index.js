9e092aadadcb2a74c1c08af6f6e647ba415d3165
NassimAlaimi <nassim.alaimi@epitech.eu>
2023-02-24 11:47:55 +0100

    [UPDATE]: new system of section

--- area/src/components/infoBlock/index.js
+++ area/src/components/infoBlock/index.js
@@ -3,41 +3,54 @@ import { InfoBlockContainer, InfoWrapper, InfoTitle, InfoAction, InputBox, Littl
 import Select from 'react-select'
 import Draggable from 'react-draggable'
 
-const DropdownSection = () => {
+const DropdownSection = (props) => {
   const options = [
     { value: 'générazdadazdazdadl', label: 'géazdazdazdazdanéral' },
   ]
 
   return (
-    <InfoAction>
-      <Select options={options} />
-    </InfoAction>
+    <InfoWrapper>
+      <InfoTitle>{props.title}</InfoTitle>
+      <InfoAction>
+        <Select options={options} />
+      </InfoAction>
+    </InfoWrapper>
   )
 }
 
 const TextSection = (props) => {
   return (
-    <InfoAction>
-      <InputBox id="mail" type="text" placeholder={props.text} />
-    </InfoAction>
+    <InfoWrapper>
+      <InfoTitle>{props.title}</InfoTitle>
+      <InfoAction>
+        <InputBox id="mail" type="text" placeholder={props.text} />
+      </InfoAction>
+    </InfoWrapper>
   )
 }
 
 const InfoBlock = (props) => {
-console.log(props.action)
+  function getFields() {
+    let fields = []
+    console.log('----------------------')
+    for (let i = 0; i < props.action.length; i++) {
+      console.log(props.action[i].fields)
+    }
+    console.log('----------------------')
+  }
+
   return (
+    useEffect(() => {
+      console.log(props.action[props.IsVisible]?.fields)
+      // getFields()
+      // console.log(props.IsVisible)
+    }, [props.IsVisible]),
     props.IsVisible !== null ? (
       <Draggable>
-        <InfoBlockContainer top={(Math.trunc(props.top)-130) + "px"} left={(Math.trunc(props.left)+50) + "px"} background={props.background}>
-          <InfoWrapper>
-            <InfoTitle>Quel message veut-tu ecrire</InfoTitle>
-            <TextSection text="Message" />
-          </InfoWrapper>
+        <InfoBlockContainer top={(Math.trunc(props.top) - 130) + "px"} left={(Math.trunc(props.left) + 50) + "px"} background={props.background}>
+          <TextSection title="Quel message veut-tu ecrire ?" text="Message" />
           <LittleBorder />
-          <InfoWrapper>
-            <InfoTitle>Dans quel channel veut tu écrire se message ?</InfoTitle>
-            <DropdownSection />
-          </InfoWrapper>
+          <DropdownSection title="Quel service veux-tu utiliser ?" />
         </InfoBlockContainer>
       </Draggable>
     ) : (<></>)
