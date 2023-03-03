4d4909c11f0a765ea7e62b12492d37ece25bb0df
Enzo Bonato <enzo.bonato@epitech.eu>
2023-03-03 17:28:57 +0100

    [ADD]: Add documentation base project

--- area/src/utils/AreasCounter.js
+++ area/src/utils/AreasCounter.js
@@ -1,13 +1,18 @@
+// save a variable in this file
+
 let AreasCounter = 0;
 
+// function to increment the variable
 export function incrementAreasCounter() {
   AreasCounter += 1;
 }
 
+// function to get the counter of the variable
 export function getAreasCounter() {
   return AreasCounter;
 }
 
+// function to reset the variable
 export function resetAreasCounter() {
   AreasCounter = 0;
 }
