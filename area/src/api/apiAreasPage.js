b5bc02a280c41a05ab8b1b3378180428ba0b069c
Enzo Bonato <enzo.bonato@epitech.eu>
2023-03-03 17:23:48 +0100

    [ADD: Documentation for the Api functions

--- area/src/api/apiAreasPage.js
+++ area/src/api/apiAreasPage.js
@@ -1,18 +1,23 @@
 import axios from "axios"
 import { useMutation } from "react-query"
 
+// function to get infos from the db about the areas
 const getInfosAreas = async () => {
     return await axios.post("/automation/get", JSON.stringify({token: sessionStorage.getItem("token")}))
 };
 
+// function which call the getinfos areas
 export const useGetInfosAreas = () => {
     return useMutation(getInfosAreas)
 };
 
+// function that makes the request to delete an area
 const deleteInfosAreas = async (data) => {
     return await axios.post("/automation/delete", JSON.stringify({automation_id: data.id}))
 };
 
+
+// function which call the deleteinfosAreas 
 export const useDeleteInfosAreas = () => {
     return useMutation(deleteInfosAreas)
 };
