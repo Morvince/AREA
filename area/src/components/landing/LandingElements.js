8b217b99277d57f963f67abac1d77c3646627e0e
noaleclaire <72016693+noaleclaire@users.noreply.github.com>
2023-03-06 11:46:15 +0100

    Revert "[ADD]: apk generate + ytb video"

--- area/src/components/landing/LandingElements.js
+++ area/src/components/landing/LandingElements.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Link, Link as LinkR } from 'react-router-dom';
+import { Link as LinkR } from 'react-router-dom';
 
 export const Rect = styled.div`
     width: 100%;
@@ -78,7 +78,7 @@ export const ButtonWithBg = styled(LinkR)`
     }
 `;
 
-export const ButtonTryIt = styled(LinkR)`
+export const ButtonTryIt = styled.button`
     position: absolute;
     display: inline-block;
     text-align: center;
